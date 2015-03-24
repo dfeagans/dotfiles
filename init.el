b5ef7c8a8f7fa64af085529afaba71da1a059272
dfeagans <dane.feagans@gmail.com>
2015-03-24 02:00:33 +0000

    Turned ido mode on because it mades find-file so much better. Removed the ffap line because it's loaded by default. I don't even use ffap though

--- init.el
+++ init.el
@@ -4,15 +4,14 @@
 ;; -- Global Settings --
 ;; ---------------------
 (add-to-list 'load-path "~/.emacs.d")
-;;(require 'cl)          ;;Includes the common lisp package
-;;(require 'ido)         ;;Interactive Do enables C-x b changes the buffer. C-x C-f does find file.
-;;(require 'ffap)        ;;Improves the power of file finds etc. Lets you used C-x d for ffap-list-directory
+;;(require 'cl)          ;;Includes the common lisp language. Not going to require unless I need it.
+(require 'ido)           ;;Interactive Do enables C-x b changes the buffer. Improves functionality of C-x C-f
 ;;(require 'dired-x)     ;;Provides methods for viewing/visiting a file mentioned in a file opened in a buffer.
 ;;(require 'uniquify)    ;;Handles when you have two files of the same name in different directories opened.
 ;;(require 'ansi-color)
 ;;NOT NEEDED(require 'recentf)    ;;lets you interact with recentfiles, I don't have any functions key-bound though.
 ;;(require 'compile)                 ;; !!!!MIGHT BE NEEDED FOR THE JS-MODE!!!!
-;;(ido-mode t)                         ;; actually turns on ido-mode
+(ido-mode t)                         ;; actually turns on ido-mode
 (setq inhibit-startup-message t)     ;; turns off startup message
 (menu-bar-mode -1)                   ;; turns the top and bottom menus off because I won't need them in the terminal
 (setq suggest-key-bindings t)        ;; If you run a command using M-x COMMAND, it will show the shortcut on the mini-buffer afterwards
