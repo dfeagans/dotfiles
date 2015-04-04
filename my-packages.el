f9fe5b2dc081e932e4b76d36921a4b964881986c
dfeagans <dane.feagans@gmail.com>
2015-04-04 14:23:15 +0000

    made ac-mode work in all buffers. Working to make cleaner, global-auto-complete-mode t should have already been called in ac-config-default.

--- my-packages.el
+++ my-packages.el
@@ -52,7 +52,7 @@
     (setq yas-dont-activate t)))
 ;; ---- The below was commented out because once you add yasnippets as a source to auto-complete, ac-complete (tab) automatically expands the snippet. ----
 ;;(define-key yas-minor-mode-map (kbd "TAB")    nil)       ;; the tab button is better used for auto-complete, so this stops yasnippet from using it. "TAB" changes both TAB and C-i, so works in terminals.
-;;(define-key yas-minor-mode-map (kbd "\C-o") 'yas-expand) ;; makes C-o expanding snippets.
+;;(define-key yas-minor-mode-map (kbd "\C-o") 'yas-expand) ;; makes C-o expanding snippets. Original plan was to totally separate yasnippet and auto-complete.
 ;;(define-key yas-minor-mode-map (kbd "<tab>")  nil)       ;; This is the tab-key in the graphical terminal (separate from C-i), and therefore not needed since I only use the terminal.
 
 (require 'auto-complete-config)                       ;; This requires 'auto-complete, so I removed that step. It also includes already includes all the yasnippets
@@ -64,3 +64,4 @@
 (define-key ac-complete-mode-map "\t" 'ac-complete)   ;; These two lines make tab immedietely complete the auto-complete, it's necessary because even with ac-dwim, tab starts stepping through the list.
 (define-key ac-complete-mode-map [tab] 'ac-complete)  ;; ac-dwim only ac-completes after you've adjusted the cursor. that means for the first one it's a pain.
 (setq-default ac-sources (push 'ac-source-yasnippet ac-sources)) ;; adds yasnippets as a source for autocomplete by default.
+(global-auto-complete-mode t)                         ;; This is already called in ac-config-default, but adding it here makes auto-complete mode actually work in all buffers.
